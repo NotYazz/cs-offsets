3905a043f3074e2b77a6bcd7c802e13a9e211a1d
Yazz <notyazz.dll@gmail.com>
2025-03-23 00:23:53 -0500

    game build -> 17800215

---
+++ offsets/buttons.cs
@@ -0,0 +1,24 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-03-23 05:22:39.903484800 UTC
+
+namespace CS2Dumper {
+    // Module: client.dll
+    public static class Buttons {
+        public const nint attack = 0x1885770;
+        public const nint attack2 = 0x1885800;
+        public const nint back = 0x1885A40;
+        public const nint duck = 0x1885D10;
+        public const nint forward = 0x18859B0;
+        public const nint jump = 0x1885C80;
+        public const nint left = 0x1885AD0;
+        public const nint lookatweapon = 0x1AAE3D0;
+        public const nint reload = 0x18856E0;
+        public const nint right = 0x1885B60;
+        public const nint showscores = 0x1AAE2B0;
+        public const nint sprint = 0x1885650;
+        public const nint turnleft = 0x1885890;
+        public const nint turnright = 0x1885920;
+        public const nint use = 0x1885BF0;
+        public const nint zoom = 0x1AAE340;
+    }
+}
---
+++ offsets/buttons.rs
@@ -0,0 +1,26 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-03-23 05:22:39.903484800 UTC
+
+#![allow(non_upper_case_globals, unused)]
+
+pub mod cs2_dumper {
+    // Module: client.dll
+    pub mod buttons {
+        pub const attack: usize = 0x1885770;
+        pub const attack2: usize = 0x1885800;
+        pub const back: usize = 0x1885A40;
+        pub const duck: usize = 0x1885D10;
+        pub const forward: usize = 0x18859B0;
+        pub const jump: usize = 0x1885C80;
+        pub const left: usize = 0x1885AD0;
+        pub const lookatweapon: usize = 0x1AAE3D0;
+        pub const reload: usize = 0x18856E0;
+        pub const right: usize = 0x1885B60;
+        pub const showscores: usize = 0x1AAE2B0;
+        pub const sprint: usize = 0x1885650;
+        pub const turnleft: usize = 0x1885890;
+        pub const turnright: usize = 0x1885920;
+        pub const r#use: usize = 0x1885BF0;
+        pub const zoom: usize = 0x1AAE340;
+    }
+}
